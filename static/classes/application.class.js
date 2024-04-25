7fbfddc0093756888ff673f31703e5cc5315f6f5
PRdWit <P.D.business@outlook.com>
2024-04-25 15:13:14 +0200

    Created Method updateDom()

--- static/classes/application.class.js
+++ static/classes/application.class.js
@@ -1,3 +1,30 @@
 class Application{
-    
+    $loggedIn;
+
+
+    constructor(){
+        this.$loggedIn = true;
+        console.log("Testing")
+    }
+
+    updateDom(){
+        const logout = document.querySelector(".logout");
+        const login = document.querySelector(".login")
+        const profile = document.querySelector(".profile")
+        const register = document.querySelector(".register")
+        if(this.$loggedIn == true ){
+            login.style.display = "none"
+            register.style.display = "none"
+
+        } else {
+            logout.style.display = "none";
+            profile.style.display = "none"
+            register.style.display
+        }
+    }
 }
+
+
+
+const app = new Application();
+app.updateDom();
